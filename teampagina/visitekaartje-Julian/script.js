74623849ea00fa9dc2ac653750f43a63d3c34662
juliandecloe <70694320+juliandecloe@users.noreply.github.com>
2022-02-11 12:23:11 +0100

    Delete teampagina/visitekaartje-Julian directory

--- teampagina/visitekaartje-Julian/script.js
+++
@@ -1,9 +0,0 @@
-const backgroundNormal = document.querySelector('.bg1');
-const backgroundFunny = document.querySelector('.bg2');
-
-function showFunnypic() {
-	backgroundFunny.classList.remove('hide');
-	setTimeout(function() { backgroundFunny.classList.add('hide'); }, 500);
-}
-
-backgroundNormal.addEventListener('click', showFunnypic);
